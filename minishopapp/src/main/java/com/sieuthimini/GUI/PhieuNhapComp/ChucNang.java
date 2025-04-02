19a647ab1d0e52e5e5bbfc548ed593eef21d6e83
Auresol69 <quocbao962005@gmail.com>
2025-04-02 20:34:44 +0700

    update Chucnang cua phieu nhap

--- minishopapp/src/main/java/com/sieuthimini/GUI/PhieuNhapComp/ChucNang.java
+++ minishopapp/src/main/java/com/sieuthimini/GUI/PhieuNhapComp/ChucNang.java
@@ -2,6 +2,7 @@ package com.sieuthimini.GUI.PhieuNhapComp;
 
 import com.sieuthimini.ExtendClasses.GetImagePNG;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -13,26 +14,76 @@ import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
 public class ChucNang extends JPanel implements ActionListener {
     JButton themButton, chiTietButton, huyButton, xuatExcelButton;
     JComboBox columnSort;
     JTextField timKiem;
-    JButton refresh;
+    JButton refreshButton;
+
+    public void setUpButton(JButton button, String text, String imgName) {
+        button.setText(text);
+        ImageIcon icon = new ImageIcon(new GetImagePNG().getImage(imgName));
+        if (icon != null) {
+            button.setIcon(icon);
+        }
+        button.setPreferredSize(new Dimension(120, 120));
+        button.setFocusable(false);
+        if (!button.equals(refreshButton)) {
+            button.setHorizontalTextPosition(JButton.CENTER);
+            button.setVerticalTextPosition(JButton.BOTTOM);
+        } else {
+            button.setHorizontalTextPosition(JButton.RIGHT);
+        }
+        button.addActionListener(this);
+        this.add(button);
+    }
 
     public ChucNang() {
         this.setLayout(new FlowLayout());
 
-        chiTietButton = new JButton("Chi tiết");
-        huyButton = new JButton("Hủy");
-        xuatExcelButton = new JButton("Xuất Excel");
+        themButton = new JButton();
+        chiTietButton = new JButton();
+        huyButton = new JButton();
+        xuatExcelButton = new JButton();
+        refreshButton = new JButton();
 
-        themButton = new JButton("Thêm", new ImageIcon(new GetImagePNG().getImage("square-plus-solid.png")));
-        themButton.setPreferredSize(new Dimension(120, 120));
-        themButton.setFocusable(false);
-        themButton.setHorizontalTextPosition(JButton.CENTER);
-        themButton.setVerticalTextPosition(JButton.BOTTOM);
-        themButton.addActionListener(this);
-        this.add(themButton);
+        setUpButton(themButton, "Thêm", "square-plus-solid.png");
+        setUpButton(chiTietButton, "Thông tin", "circle-info-solid.png");
+        setUpButton(huyButton, "Hủy", "circle-xmark-solid.png");
+        setUpButton(xuatExcelButton, "Xuất Excel", "file-excel-solid.png");
+
+        String options[] = { "tất cả", "mã phiếu", "nhân viên", "nhà cung cấp" };
+        columnSort = new JComboBox<>(options);
+        columnSort.addActionListener(this);
+        this.add(columnSort);
+
+        timKiem = new JTextField("Nhập nội dung...");
+        timKiem.setForeground(Color.GRAY);
+
+        timKiem.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (timKiem.getText().equals("Nhập nội dung...")) {
+                    timKiem.setText("");
+                    timKiem.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (timKiem.getText().isEmpty()) {
+                    timKiem.setText("Nhập nội dung...");
+                    timKiem.setForeground(Color.GRAY);
+                }
+            }
+        });
+        this.add(timKiem);
+
+        setUpButton(refreshButton, "Làm mới", "arrows-rotate-solid.png");
+
     }
 
     @Override
--- minishopapp/src/main/java/com/sieuthimini/GUI/PhieuNhapComp/NhapLieu.java
+++ minishopapp/src/main/java/com/sieuthimini/GUI/PhieuNhapComp/NhapLieu.java
@@ -1,5 +1,15 @@
 package com.sieuthimini.GUI.PhieuNhapComp;
 
-public class NhapLieu {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class NhapLieu extends JPanel {
+
+    JLabel suplierLabel, staffLabel, fromLabel, toLabel, fromMoneyLabel, toMoneyLabel;
+    JTextField suplierField, staffField, fromField, toField, fromMoneyField, toMoneyField;
+
+    public NhapLieu() {
+
+    }
 }
